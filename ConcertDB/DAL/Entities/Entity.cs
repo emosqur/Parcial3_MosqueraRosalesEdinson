eb254365d509f45423013a6a4c7d456b36ca5bce
edinson rosales <edidy9v@gmail.com>
2023-11-11 18:34:29 -0500

    DataBaseContext

--- ConcertDB/DAL/Entities/Entity.cs
+++ ConcertDB/DAL/Entities/Entity.cs
@@ -1,10 +1,18 @@
-﻿namespace ConcertDB.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
 {
     public class Entity
     {
+        [Required]
       public  Guid Id { get; set; }
-     
-     
+
+        [Display(Name = "Fecha de creacion")]
+      public DateTime? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de Modificacion")]
+        public DateTime? ModifiedDate { get; set;}
+                    
 
     }
 }
--- ConcertDB/DAL/Entities/Tickets.cs
+++ ConcertDB/DAL/Entities/Tickets.cs
@@ -1,4 +1,6 @@
-﻿namespace ConcertDB.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
 {
     public class Tickets:Entity
     {
@@ -6,6 +8,9 @@
 
         public Boolean IsUsed { get; set;}
 
+        [Display(Name ="Entrada")]
+        [Required]
+        [MaxLength(50, ErrorMessage ="El campo{0} debe tener maximo{1} caracteres.")]
         public String EntranceGate { get; set; }
 
 
