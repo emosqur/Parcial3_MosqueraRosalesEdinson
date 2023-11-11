9b9818e11c2070fc2596c8596c9265f48eaa7be3
edinson rosales <edidy9v@gmail.com>
2023-11-11 17:20:34 -0500

    the Tickets table is created

---
+++ ConcertDB/DAL/Entities/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace ConcertDB.DAL.Entities
+{
+    public class Entity
+    {
+      public  Guid Id { get; set; }
+     
+     
+
+    }
+}
---
+++ ConcertDB/DAL/Entities/Tickets.cs
@@ -0,0 +1,13 @@
+﻿namespace ConcertDB.DAL.Entities
+{
+    public class Tickets:Entity
+    {
+        public DateTime? UseDate { get; set; }
+
+        public Boolean IsUsed { get; set;}
+
+        public String EntranceGate { get; set; }
+
+
+    }
+}
