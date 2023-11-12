390031f006ef82891988513f8bbfeebed2347002
edinson rosales <edidy9v@gmail.com>
2023-11-12 09:52:31 -0500

    the 1000 records are created automatically

---
+++ ConcertDB/DAL/SeederDB.cs
@@ -0,0 +1,36 @@
+﻿using ConcertDB.DAL.Entities;
+
+namespace ConcertDB.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+        public SeederDb(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            PopulateTickets(); 
+            await _context.SaveChangesAsync();
+        }
+
+        private void PopulateTickets()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 1; i <= 1000; i++)
+                {
+                    _context.Tickets.Add(new Entities.Tickec
+                    {
+                        TicketID = i, UseDate = null,IsUsed = false,EntranceGate = null,CreateDate = DateTime.Now,
+                    }
+                    );
+                }
+
+            }
+        }
+    }
+}
