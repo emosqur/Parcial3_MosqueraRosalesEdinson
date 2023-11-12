1dc2e3a73b2b0b35c85ac652998ebb77fb11487f
edinson rosales <edidy9v@gmail.com>
2023-11-11 20:09:46 -0500

    create connection string

--- ConcertDB/DAL/DataBaseContext.cs
+++ ConcertDB/DAL/DataBaseContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ConcertDB.DAL
+﻿using ConcertDB.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcertDB.DAL
 {
-    public class DataBaseContext
+    public class DataBaseContext:DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+             
+        }
+        public DbSet<Tickets> Ticketss{ get; set; }
     }
 }
